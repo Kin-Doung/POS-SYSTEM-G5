ca4c38abdce7f0b1124ea536c26eb6e8b584f516
Phally-Chheang <phally.chheang@student.passerellesnumeriques.org>
2025-04-02 10:13:52 +0700

    Edit logo: from # 23

--- Views/layouts/side.php
+++ Views/layouts/side.php
@@ -2,7 +2,7 @@
   <div class="sidenav-header ms-0 my-0">
     <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
     <a class="navbar-brand m-0" href="#" target="_blank">
-      <img src="../../views/assets/img/logos/Engly-Logo.png" class="navbar-brand-img h-100" alt="main_logo">
+    <img src="../../views/assets/img/logos/Engly-Logo.png" class="navbar-brand-img" style="height: 100px; width: 100px;" alt="main_logo">
     </a>
   </div>
   <hr class="horizontal dark mt-0 mb-2">
